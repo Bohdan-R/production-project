1cbd4470d33e267caa545bd8ff48734ee0bb5135
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2022-09-21 13:18:50 +0300

    create AppRouter config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,21 @@
-import React, { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import './styles/index.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/index.scss";
 import { useTheme } from "app/providers/ThemeProvider";
-import { classNames } from 'shared/lib/classNames/classNames';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/Router";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
 
-    const { theme, toggleTheme } = useTheme();
-  
-    return (
-      <div className={classNames('app', {}, [theme])}>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-        <Link to={"/"}>Main</Link>
-        <Link to={"/about"}>About</Link>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path={"/about"} element={<AboutPage />} />
-            <Route path={"/"} element={<MainPage />} />
-          </Routes>
-        </Suspense>
-      </div>
-    );
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <AppRouter />
+    </div>
+  );
 };
 
 export default App;
