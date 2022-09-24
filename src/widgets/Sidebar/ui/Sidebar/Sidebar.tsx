02277a520b9dcf05c7b7b3011515cb9b70885b6f
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2022-09-24 11:09:22 +0300

    Add Sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useState } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import styles from "./Sidebar.module.scss";
+
+interface ISidebarProps {
+  className?: string;
+}
+
+export const Sidebar: FC<ISidebarProps> = ({ className }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed(!collapsed);
+  };
+  return (
+    <div
+      className={classNames(styles.sidebar, { [styles.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <button onClick={onToggle}>Toggle</button>
+    </div>
+  );
+};
