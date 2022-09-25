65dcccabc943c7d694f8946827e5de643b6fe554
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2022-09-25 08:50:08 +0300

    Add webpack hot module replacement

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -2,9 +2,10 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 import { IBuildOptions } from "./types/config";
 
 export function buildDevServer(options: IBuildOptions): DevServerConfiguration {
-    return {
-        port: options.port,
-        open: true,
-        historyApiFallback: true,
-  }
+  return {
+    port: options.port,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  };
 }
